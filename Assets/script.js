0f5937f73ef41b3b8c50ea137d5c78509f86e702
Lawrence Otieno <lawrence.otieno@student.moringaschool.com>
2021-07-19 06:37:02 +0300

    added female names

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,4 @@
 var akanNames = function(){
     var mNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var fmNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 }

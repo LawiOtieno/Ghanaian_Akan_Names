fcfa41831958dd1fda51487ff2c14e9bce166597
Lawrence Otieno <lawrence.otieno@student.moringaschool.com>
2021-07-19 06:38:26 +0300

    added days of week

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,5 @@
 var akanNames = function(){
     var mNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var fmNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 }

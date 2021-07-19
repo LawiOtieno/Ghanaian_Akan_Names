acc2038c1bb07ef169d2f80dfed8a639cfdb327c
Lawrence Otieno <lawrence.otieno@student.moringaschool.com>
2021-07-19 06:40:47 +0300

    function to verify day

--- Assets/script.js
+++ Assets/script.js
@@ -2,4 +2,9 @@ var akanNames = function(){
     var mNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var fmNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    // function to verify day
+    var invalidDay = function(dd){
+        return dd <= 0 || dd > 31;
+    };
 }
